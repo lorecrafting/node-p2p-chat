d15b2143d3413a9f0e4c490894bd8278dfef013d
Raymond Luong <raymondluong@Raymonds-MacBook-Pro.local>
2018-08-04 18:50:21 -1000

    initial ping

--- p2pchat.js
+++ p2pchat.js
@@ -3,7 +3,7 @@ const net = require("net");
 const ENTRY_PEER = process.env.ENTRY_PEER;
 
 let isGenesisNode = true;
-const peerList = [];
+const peerList = {};
 
 if (ENTRY_PEER) {
   isGenesisNode = false;
@@ -16,12 +16,22 @@ const server = net.createServer(socket => {
   const socketAddr = socket.address().address.split(":");
   const peerIp = socketAddr[3];
 
-  peerList.push(peerIp);
+  peerList[peerIp] = true;
+  peerList.forEach(peerAddr => {
+    net.createConnection(6060, peerIp, socket => {
+      socket.write(`/addpeer ${peerIp}`);
+    });
+  });
+  console.log("PeerList after Adding: ", peerList);
   socket.write("You have connected to node-p2p-chat system...");
 
   console.log("Connected: ", socketAddr);
   socket.on("data", data => {
     const msg = data.toString();
+    if (msg.includes("/addpeer")) {
+      const newPeerIp = msg.split(" ")[1];
+      peerList[newPeerIp] = true;
+    }
     console.log(msg);
     peerList.forEach(peerIp => {
       net.createConnection(6969, peerIp, socket => {
